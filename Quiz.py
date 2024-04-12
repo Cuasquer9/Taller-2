6d4f7ae73a4e6bc3d7066f6edb5c7ebf693bc614
Cuasquer9 <cristhian.cuasquer@udea.edu.co>
2024-04-12 11:29:46 -0500

    Punto 5 y 6

--- Quiz.py
+++ Quiz.py
@@ -9,13 +9,28 @@ matriz_4d = np.random.rand(15,20,40,100)
 matriz_3d = matriz_4d.copy()[:, :, 0]
 
 #punto 3
-print("Atributos de la matriz 3d: ")
+print("Atributos de la Matriz 3D: ")
 print("Dimensiones: ", matriz_3d.ndim)
-print("Tamaño: ", matriz_3d.size)
-print("Forma: ", matriz_3d.shape)
+print("Tamaño 3D: ", matriz_3d.size)
+print("Forma 3D: ", matriz_3d.shape)
 print("Tipo de datos: ", matriz_3d.dtype)
 
 #punto 4
 matriz_2d = matriz_3d.reshape((15*20,100))
-print("Tamaño: ", matriz_2d.size)
-# print("Forma: ", matriz_2d.shape)
+print("Atributos Matriz 2D")
+print("Dimensiones: ", matriz_2d.ndim)
+print("Tamaño 2D: ", matriz_2d.size)
+print("Forma 2D: ", matriz_2d.shape)
+
+#punto 5
+def matriz_a_df(matriz_2d):
+    df = pd.DataFrame(matriz_2d)
+    return df
+
+#punto 6
+def cargar_archivo_mat(archivo):
+    datos = sio.loadmat(archivo)
+    return datos
+def cargar_archivo_csv(archivo):
+    datos = pd.read_csv(archivo)
+    return datos
